fec981296c5f3d9216ec7c60c93dc027bc337429
damcik <suchuschy@gmail.com>
2025-01-14 15:36:24 +0100

    make seed.sh executable

--- tests/data/seed/seed.sh
+++ tests/data/seed/seed.sh
@@ -6,7 +6,4 @@ POS_ENV="${1:-$DEFAULT_ENV}"
 pos-cli data clean $POS_ENV --auto-confirm --include-schema
 
 pos-cli deploy $POS_ENV
-cd ./tests/post_import
-env CONFIG_FILE_PATH=./../../.pos pos-cli data import --path=./../data/seed/data.zip --zip $POS_ENV
-env CONFIG_FILE_PATH=./../../.pos pos-cli deploy -p $POS_ENV
-cd -
+pos-cli data import --path=./../data/seed/data.zip --zip $POS_ENV
