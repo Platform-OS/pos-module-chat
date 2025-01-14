ed24d2d79c5077ed0364f992fa8c68c40e12db25
damcik <suchuschy@gmail.com>
2025-01-14 15:04:45 +0100

    add playwright

---
+++ tests/data/users.ts
@@ -0,0 +1,12 @@
+export const users = {
+    test1: {
+      email: 'test1@example.com',
+      firstName: 'Aaron',
+      lastName: 'Lopez'
+    },
+    test2: {
+      email: 'test2@example.com',
+      firstName: 'Rhonda',
+      lastName: 'Blackwell'
+  }
+};
