62a78d5c600938f09adbe28757595fc915168fd1
damcik <suchuschy@gmail.com>
2025-01-14 16:25:56 +0100

    fix path

--- tests/data/seed/seed.sh
+++ tests/data/seed/seed.sh
@@ -6,4 +6,5 @@ POS_ENV="${1:-$DEFAULT_ENV}"
 pos-cli data clean $POS_ENV --auto-confirm --include-schema
 
 pos-cli deploy $POS_ENV
-pos-cli data import --path=../data/seed/data.zip --zip $POS_ENV
+
+pos-cli data import --path=./tests/data/seed/data.zip --zip $POS_ENV
