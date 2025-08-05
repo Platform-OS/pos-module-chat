ac9ded2f5970237550188681777d0d9536834ad2
Damian <damian@platformos.com>
2025-08-05 19:47:51 +0200

    update seed.sh

--- tests/data/seed/seed.sh
+++ tests/data/seed/seed.sh
@@ -6,5 +6,8 @@ POS_ENV="${1:-$DEFAULT_ENV}"
 pos-cli data clean $POS_ENV --auto-confirm --include-schema
 
 pos-cli deploy $POS_ENV
+cd ./tests/post_import
 
-pos-cli data import --path=./tests/data/seed/data.zip --zip $POS_ENV
+env CONFIG_FILE_PATH=./../../.pos pos-cli data import --path=./../data/seed/data.zip --zip $POS_ENV
+env CONFIG_FILE_PATH=./../../.pos pos-cli deploy -p $POS_ENV
+cd -
